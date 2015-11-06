913ea24746b10642b35883df28121255b25d3f03
Java-man <java.deveveloper@gmail.com>
2015-11-06 19:27:20 +0300

    loginserver -> authserver

---
+++ authserver/src/main/scala/ru/jts/authserver/AuthServer.scala
@@ -0,0 +1,11 @@
+package ru.jts.authserver
+
+/**
+  * @author Camelion
+  * @since 06.11.15
+  */
+object AuthServer extends App {
+
+
+
+}
--- loginserver/src/main/scala/ru/jts/loginserver/LoginserverApp.scala
+++
@@ -1,9 +0,0 @@
-package ru.jts.loginserver
-
-/**
-  * @author Camelion
-  * @since 06.11.15
-  */
-object LoginserverApp extends App {
-
-}
