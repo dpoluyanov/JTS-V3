7d980806a1d522224effc3ab93ee9709c5b73e3d
Camelion <camelion.c@mail.ru>
2015-11-06 11:33:42 +0500

    спринг минус

--- loginserver/src/main/scala/ru/jts/loginserver/LoginserverApp.scala
+++ loginserver/src/main/scala/ru/jts/loginserver/LoginserverApp.scala
@@ -1,21 +1,9 @@
 package ru.jts.loginserver
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.{ApplicationArguments, ApplicationRunner, SpringApplication}
-
 /**
   * @author Camelion
   * @since 06.11.15
   */
-class AppConfig
+object LoginserverApp extends App {
 
-@SpringBootApplication
-object LoginserverApp extends App with ApplicationRunner {
-
-  // context will be initialized after run method
-  var context = SpringApplication.run(classOf[AppConfig])
-
-  override def run(args: ApplicationArguments): Unit = {
-
-  }
 }
