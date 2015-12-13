5ff17913773bca507263977ca26605e5b491d223
Java-man <java.deveveloper@gmail.com>
2015-12-13 20:56:39 +0300

    хрень

---
+++ client-emulator/src/main/java/ru/jts/client_emulator/ClientEmulatorApplication.java
@@ -0,0 +1,16 @@
+package ru.jts.client_emulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Java-man
+ * @since 08.12.2015
+ */
+@SpringBootApplication
+public class ClientEmulatorApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(ClientEmulatorApplication.class, args);
+	}
+}
