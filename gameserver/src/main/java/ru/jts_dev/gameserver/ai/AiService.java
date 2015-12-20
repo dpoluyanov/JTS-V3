3ae4ba8755e7b91fcb29bfd4af3e51401edcb3a1
Java-man <java.deveveloper@gmail.com>
2015-12-21 01:19:22 +0300

    вот так вот

--- gameserver/src/main/java/ru/jts_dev/gameserver/ai/AiService.java
+++ gameserver/src/main/java/ru/jts_dev/gameserver/ai/AiService.java
@@ -1,19 +1,23 @@
 package ru.jts_dev.gameserver.ai;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import reactor.Environment;
 import reactor.rx.broadcast.Broadcaster;
 import ru.jts_dev.gameserver.ai.tasks.Tasks;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author Java-man
  * @since 13.12.2015
  */
-@Component
+@Service
 public class AiService {
     private final Broadcaster<InternalMessage> broadcaster = Broadcaster.create();
 
-    public AiService() {
+    @PostConstruct
+    private void startBroadCaster() {
         broadcaster
                 // dispatch onto a Thread other than 'main'
                 .dispatchOn(Environment.cachedDispatcher())
