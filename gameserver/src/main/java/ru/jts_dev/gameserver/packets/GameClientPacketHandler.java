15d1fa741f9e08c7409a8c020f60e79f468456a9
Camelion <camelion.c@mail.ru>
2016-01-06 16:55:58 +0500

    New Packet handling system

--- gameserver/src/main/java/ru/jts_dev/gameserver/packets/GameClientPacketHandler.java
+++ gameserver/src/main/java/ru/jts_dev/gameserver/packets/GameClientPacketHandler.java
@@ -9,7 +9,6 @@ import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 import ru.jts_dev.common.packets.IncomingMessageWrapper;
-import ru.jts_dev.gameserver.packets.in.*;
 
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
@@ -63,60 +62,23 @@ public class GameClientPacketHandler {
             throw new RuntimeException("At least 1 readable byte excepted in buffer");
 
         int opcode = buf.readUnsignedByte();
+
+        if (!packets.containsKey(opcode))
+            throw new RuntimeException("Invalid first packet opcode: " + String.format("0x%02X", (byte) opcode));
+
         IncomingMessageWrapper msg;
-        switch (opcode) {
-            case 0x00:
-                msg = context.getBean(Logout.class);
-                break;
-            case 0x0C:
-                msg = context.getBean(CharacterCreate.class);
-                break;
-            case 0x0E:
-                msg = context.getBean(RequestProtocolVersion.class);
-                break;
-            case 0x0F:
-                msg = context.getBean(MoveBackwardToLocation.class);
-                break;
-            case 0x11:
-                msg = context.getBean(EnterWorld.class);
-                break;
-            case 0x12:
-                msg = context.getBean(CharacterSelect.class);
-                break;
-            case 0x13:
-                msg = context.getBean(RequestNewCharacter.class);
-                break;
-            case 0x2B:
-                msg = context.getBean(AuthLogin.class);
-                break;
-            case 0x67:
-                msg = context.getBean(RequestPledgeCrest.class);
-                break;
-            case 0x92:
-                msg = context.getBean(RequestAllyCrest.class);
-                break;
-            // second level opcodes
-            case 0xD0:
-                opcode = buf.readUnsignedShort();
-                switch (opcode) {
-                    case 0x01:
-                        msg = context.getBean(RequestManorList.class);
-                        break;
-                    case 0x21:
-                        msg = context.getBean(RequestKeyMapping.class);
-                        break;
-                    case 0x36:
-                        msg = context.getBean(GotoLobby.class);
-                        break;
-                    case 0x3D:
-                        msg = context.getBean(RequestShortCutReg.class);
-                        break;
-                    default:
-                        throw new RuntimeException("Invalid second packet opcode: " + String.format("0x%02X", (byte) opcode));
-                }
-                break;
-            default:
-                throw new RuntimeException("Invalid first packet opcode: " + String.format("0x%02X", (byte) opcode));
+
+        Object node = packets.get(opcode);
+        if (node instanceof Class) {
+            msg = context.getBean((Class<IncomingMessageWrapper>) node);
+        } else { // (node instanceof Map)
+            opcode = buf.readUnsignedShort();
+
+            if (!((Map) node).containsKey(opcode))
+                throw new RuntimeException("Invalid second packet opcode: " + String.format("0x%02X", (byte) opcode));
+
+            node = ((Map) node).get(opcode);
+            msg = context.getBean((Class<IncomingMessageWrapper>) node);
         }
 
         ByteBuf data = buf.slice();
