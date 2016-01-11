b9914aae0acb8e85cceb6f2cbb3ed2543983db2e
Java-man <java.deveveloper@gmail.com>
2016-01-12 00:48:16 +0300

    CharacterCreateSuccess is now static packet

--- gameserver/src/main/java/ru/jts_dev/gameserver/packets/out/CharacterCreateSuccess.java
+++ gameserver/src/main/java/ru/jts_dev/gameserver/packets/out/CharacterCreateSuccess.java
@@ -7,9 +7,15 @@ import ru.jts_dev.common.packets.OutgoingMessageWrapper;
  * @since 20.12.15
  */
 public class CharacterCreateSuccess extends OutgoingMessageWrapper {
+    public static final CharacterCreateSuccess PACKET = new CharacterCreateSuccess();
+
+    private CharacterCreateSuccess() {
+    }
+
     @Override
     public void write() {
         putByte(0x0F);
+
         putInt(0x01);
     }
 }
