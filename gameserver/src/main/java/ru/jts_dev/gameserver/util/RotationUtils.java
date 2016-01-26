fe425c7949b08643c78afc4b2bf6a94db349ac10
Java-man <java.deveveloper@gmail.com>
2016-01-26 03:06:05 +0300

    remove magic numbers in RotationUtils

--- gameserver/src/main/java/ru/jts_dev/gameserver/util/RotationUtils.java
+++ gameserver/src/main/java/ru/jts_dev/gameserver/util/RotationUtils.java
@@ -1,6 +1,7 @@
 package ru.jts_dev.gameserver.util;
 
 import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.springframework.stereotype.Component;
 
 /**
@@ -8,16 +9,19 @@ import org.springframework.stereotype.Component;
  * @since 26.01.2016
  */
 @Component
-public class RotationUtils {
+public final class RotationUtils {
+    private static final int CIRCLE_DEGREES = 360;
+    private static final double HEADING_PER_DEGREE = 65536.0D / 360.0D;    // 182.044444444
+
     public int convertAngleToClientHeading(double angle) {
         if (angle < 0)
-            angle = 360 + angle;
-        return (int) (angle * 182.044444444);
+            angle = CIRCLE_DEGREES + angle;
+        return (int) (angle * HEADING_PER_DEGREE);
     }
 
 
     public double convertClientHeadingToAngle(int heading) {
-        return heading / 182.044444444;
+        return heading / HEADING_PER_DEGREE;
     }
 
     public Rotation clientRotation(Rotation rotation, double angle, int side) {
@@ -28,10 +32,12 @@ public class RotationUtils {
     }
 
     public Rotation rotateOnLeft(Rotation rotation, double angle) {
-        return rotation.applyTo(new Rotation(rotation.getAxis(), angle));
+        Vector3D axis = rotation.getAxis();
+        return rotation.applyTo(new Rotation(axis, angle));
     }
 
     public Rotation rotateOnRight(Rotation rotation, double angle) {
-        return rotation.applyInverseTo(new Rotation(rotation.getAxis(), angle));
+        Vector3D axis = rotation.getAxis();
+        return rotation.applyInverseTo(new Rotation(axis, angle));
     }
 }
