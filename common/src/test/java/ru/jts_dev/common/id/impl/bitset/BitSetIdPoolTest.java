227a33f35155d13507fb4a6249f446daa9d5675f
Java-man <java.deveveloper@gmail.com>
2016-07-29 18:37:33 +0300

    common:   - update spring to 5.0.0.M1   - update junit to 5.0.0.M2

--- common/src/test/java/ru/jts_dev/common/id/impl/bitset/BitSetIdPoolTest.java
+++ common/src/test/java/ru/jts_dev/common/id/impl/bitset/BitSetIdPoolTest.java
@@ -1,13 +1,13 @@
 package ru.jts_dev.common.id.impl.bitset;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.jts_dev.common.config.UtilsConfig;
 import ru.jts_dev.common.id.IdPool;
@@ -15,40 +15,36 @@ import ru.jts_dev.common.id.impl.AllocationException;
 
 import java.util.stream.IntStream;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.expectThrows;
 
 /**
  * @author Camelion
  * @since 16.07.16
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {BitSetIdPool.class, UtilsConfig.class})
+@SpringJUnitConfig({BitSetIdPool.class, UtilsConfig.class})
 @TestPropertySource(properties = {"jts.common.bitset.max-size=20000"})
 public class BitSetIdPoolTest {
-    @Rule
-    public ExpectedException expectedEx = ExpectedException.none();
-
     @Autowired
     private IdPool idPool;
 
     @DirtiesContext
     @Test
-    public void testIdBorrow() throws Exception {
+    public void testIdBorrow() {
         IntStream.rangeClosed(1, 10_000).forEach(value -> {
             final int id = idPool.borrow();
-            assertThat(id, equalTo(value));
+            assertThat(id).isEqualTo(value);
         });
 
         IntStream.rangeClosed(1, 10_000).parallel().forEach(value -> {
             final int id = idPool.borrow();
-            assertThat(id, greaterThanOrEqualTo(10_000));
+            assertThat(id).isGreaterThanOrEqualTo(10_000);
         });
     }
 
     @DirtiesContext
     @Test
-    public void testIdRelease() throws Exception {
+    public void testIdRelease() {
         IntStream.rangeClosed(1, 20_000).forEach(value -> idPool.borrow());
 
         IntStream.rangeClosed(1, 20_000).parallel().forEach(value -> idPool.release(value));
@@ -56,43 +52,43 @@ public class BitSetIdPoolTest {
 
     @DirtiesContext
     @Test
-    public void testIdReusing() throws Exception {
+    public void testIdReusing() {
         IntStream.rangeClosed(1, 20_000).forEach(value -> idPool.borrow());
 
         IntStream.rangeClosed(1, 20_000).parallel().forEach(value -> idPool.release(value));
 
         IntStream.rangeClosed(1, 20_000).parallel().forEach(value -> {
             int id = idPool.borrow();
-            assertThat(id, is(allOf(greaterThanOrEqualTo(1), lessThanOrEqualTo(20_000))));
+            assertThat(id).isGreaterThanOrEqualTo(1).isLessThanOrEqualTo(20_000);
         });
     }
 
     @DirtiesContext
     @Test
-    public void releaseZeroIdThrowsException() throws Exception {
-        expectedEx.expect(IndexOutOfBoundsException.class);
-        expectedEx.expectMessage(is(startsWith("index must be > 0 and <= 20000 current: 0")));
-
-        idPool.release(0);
-    }
-
-    @DirtiesContext
-    @Test
-    public void releaseNegativeIdThrowsException() throws Exception {
-        expectedEx.expect(IndexOutOfBoundsException.class);
-        expectedEx.expectMessage(is(startsWith("index must be > 0 and <= 20000 current: -1")));
-
-        idPool.release(-1);
-    }
-
-    @DirtiesContext
-    @Test
-    public void throwsExceptionIfNoFreeIndexes() throws Exception {
-        expectedEx.expect(AllocationException.class);
-        expectedEx.expectMessage(is(equalTo("No available indexes in pool")));
-
-        IntStream.rangeClosed(0, 20000).parallel().forEach(value -> {
-            idPool.borrow();
+    public void releaseZeroIdThrowsException() {
+        Throwable exception = expectThrows(IndexOutOfBoundsException.class, () -> {
+            idPool.release(0);
         });
+        assertThat(exception.getMessage()).isEqualTo("index must be > 0 and <= 20000 current: 0");
+    }
+
+    @DirtiesContext
+    @Test
+    public void releaseNegativeIdThrowsException() {
+        Throwable exception = expectThrows(IndexOutOfBoundsException.class, () -> {
+            idPool.release(-1);
+        });
+        assertThat(exception.getMessage()).isEqualTo("index must be > 0 and <= 20000 current: -1");
+    }
+
+    @DirtiesContext
+    @Test
+    public void throwsExceptionIfNoFreeIndexes() {
+        Throwable exception = expectThrows(AllocationException.class, () -> {
+            IntStream.rangeClosed(0, 20000).parallel().forEach(value -> {
+                idPool.borrow();
+            });
+        });
+        assertThat(exception.getMessage()).isEqualTo("No available indexes in pool");
     }
 }
